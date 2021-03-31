7d8a31c8e473c50a7712824c28ce47799f8218c7
BurnySc2 <gamingburny@gmail.com>
2021-03-31 19:27:54 +0200

    Fix test command for windows, and shared lib file ending for mac

--- run_shared_lib.py
+++ run_shared_lib.py
@@ -1,4 +1,16 @@
 import ctypes
+import platform
 
-mylib = ctypes.CDLL("./libmain.so")
+if platform.system() == "Linux":
+    filename = "libmain.so"
+elif platform.system() == "Darwin":
+    filename = "libmain.dylib"
+elif platform.system() == "Windows":
+    filename = "libmain.dll"
+else:
+    filename = ""
+assert filename, "Unknown platform"
+
+mylib = ctypes.CDLL(f"./{filename}")
 print(mylib.add(400, 400))  # prints 800
+assert mylib.add == 800
